f6f1834d326f93d67eeb94acc640aa1087e8caf9
Rico Raven <ricoraven28@gmail.com>
2017-07-27 14:54:48 +0800

    New and Updates [UPDATE]

--- src/pages/newslanding/newslanding.ts
+++ src/pages/newslanding/newslanding.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
-import { IonicPage, Content, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { Http, RequestOptions, Headers, URLSearchParams } from '@angular/http';
+import { IonicPage, Content, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { TheSpeakersPage } from '../thespeakers/thespeakers';
 import { allAboutPage } from '../allabout/allabout';
 import { NewsPage } from '../news/news';
@@ -9,13 +10,47 @@ import { NewsPage } from '../news/news';
   selector: 'page-newslanding',
   templateUrl: 'newslanding.html'
 })
-export class NewslandingPage {
+export class NewslandingPage implements OnInit{
+ @ViewChild(Content) content: Content;
+myNews = [];
 allAboutPage = allAboutPage;
 TheSpeakersPage = TheSpeakersPage;
 NewsPage = NewsPage;
-  constructor() {}
+  constructor(public navCtrl: NavController, private http: Http, private loadingController: LoadingController) {
+    let loadingPopup = this.loadingController.create({
+      content: 'Verifying...'
+    });
+    loadingPopup.present();
 
-  @ViewChild(Content) content: Content;
+    let body = new URLSearchParams();
+    body.set('action', 'getOldsNews');
+    body.set('count', '10');
+    body.set('page', '1');
+    body.set('language', window.localStorage['mylanguage']);
+
+    let options = new RequestOptions({
+      headers: new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded'
+      })
+    });
+
+    this.http.post('http://cums.the-v.net/site.aspx', body, options)
+      .subscribe(response => {
+        this.myNews = response.json();
+      }, null, () => {
+        loadingPopup.dismiss();
+      });
+  }
+  
+
+  ngOnInit(): void{
+
+  }
+  navigateToNews(id: String) {
+    this.navCtrl.push(NewsPage, {
+      id: id
+    });
+  }
 
   scrollToTop() {
     this.content.scrollToTop();
